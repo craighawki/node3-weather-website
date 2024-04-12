8b558f94331ddd66a6d5a83037a3b17954fa3ada
Craig Hawkins <craighawki@gmail.com>
2024-04-12 13:07:11 -0700

    updated weather output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. It feels like " + body.current.feelslike + " degrees out. The wind speed is " + body.current.wind_speed + " mph, and the  humidity is " + body.current.humidity + " percent. There is a " + body.current.precip + " percent chance of rain.")
+            // console.log(body.current)
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + "° out. It feels like " + body.current.feelslike + "° out. The wind speed is " + body.current.wind_speed + " mph, and the  humidity is " + body.current.humidity + "%. There is a " + body.current.precip + "% chance of rain.")
         }
     })
 }
